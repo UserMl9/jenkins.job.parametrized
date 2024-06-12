951b3ad1556d294686cd0a84556b86d9e9aa9aad
UserMl9 <aprendizajeml9@gmail.com>
2024-06-12 10:24:10 -0300

    Create parametrizedDSL2.groovy

---
+++ parametrizedDSL2.groovy
@@ -0,0 +1,41 @@
+job('example2-job-DSL') {
+	description('Job Dsl Example for jenkins job')
+  	 scm {
+        git('https://github.com/UserMl9/jenkins.job.parametrized.git', 'main') { node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('aprendizajeml9@gmail.com')
+        }
+    }
+  	parameters {
+      	stringParam('name', defaultValue = 'User', description = 'String parameter for boolean Job')
+        choiceParam('planet', ['Mercury', 'Venus', 'Earth','Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'])
+      	booleanParam('agent', false)
+    }
+  	triggers {
+		cron('H/7 * * * *')
+    githubPush()
+	}
+  	steps {
+		shell("bash jobscript.sh")
+	}
+  	publishers {
+		mailer('aprendizajeml9@gmail.com', true, true)
+      	slackNotifier {
+          notifyAborted(true)
+          notifyEveryFailure(true)
+          notifyNotBuilt(false)
+          notifyUnstable(false)
+          notifyBackToNormal(true)
+          notifySuccess(false)
+          notifyRepeatedFailure(false)
+          startNotification(false)
+          includeTestSummary(false)
+          includeCustomMessage(false)
+          customMessage(null)
+          sendAs(null)
+          commitInfoChoice('NONE')
+          teamDomain(null)
+          authToken(null)
+        }
+	}
+}
